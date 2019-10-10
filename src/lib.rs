3757f244823f8e005c43fdeb421d0190c88f629a
Mikhail Hasso <mihasso@protonmail.com>
2019-10-10 15:12:06 +0300

    enum Answer

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-    pub use std::{
+    use std::collections::LinkedList;
+pub use std::{
         env, io,
         net::{Ipv4Addr, UdpSocket},
         collections::HashMap,
@@ -12,18 +13,29 @@
     /*pub extern crate serde_json;
     pub extern crate serde;*/
 
-
-
     #[macro_use]
     use serde_derive::*;
-    //pub extern crate serde_derive;
-
 
     #[derive(Serialize, Deserialize)]
-    enum Command{
-    Share{file_path: String,},
-    Scan{},
-    LS{file_list: HashMap<u32, String>,},
-    Download{file_index: u32,save_path: String,},
-    CmdStatus{},
-}
+    pub enum Command{   //Client -> Daemon
+        Share{file_path: String,},
+        Scan{},
+        LS{},
+        Download{file_id: u32, save_path: String,},
+        //Download{file_name: String, save_path: String,},
+        Status{},
+    }
+
+    #[derive(Serialize, Deserialize)]
+    pub enum Answer{    //Daemon -> Client
+//      Scan{}, //Just update ls results?
+        LS{file_list: LinkedList<File>,},
+        Status{download_list: LinkedList<File>, shared_list: LinkedList<String>},
+    }//shared_list - just names of files, should I use File struct too?
+
+    #[derive(Serialize, Deserialize)]
+    pub struct File{
+        file_id: u32,   //Inactive in Anwer->Status case
+        file_name: String,
+        peer: String,   //(Or Ipv4Adress) "Remote" peer or client
+    }
