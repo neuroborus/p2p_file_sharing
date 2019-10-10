9748bf37552ac300657a99b4b2894a0704b9e0af
Mikhail Hasso <mihasso@protonmail.com>
2019-10-10 19:14:05 +0300

    Prepare to writing some clap stuff

---
+++ src/client.rs
@@ -0,0 +1,26 @@
+use lib::*;
+//socket == "channel"
+
+
+
+fn main() -> io::Result<()> {   //
+    assert!(ADDR.is_multicast());   //
+
+    let message = env::args().skip(1).collect::<Vec<_>>().join(" ");    //+
+    //let message: Vec<String> = env::args().skip(1).collect();
+    //String s;
+
+
+    let socket = UdpSocket::bind((Ipv4Addr::new(0, 0, 0, 0), 0))?;  //
+    socket
+        .send_to(message.as_bytes(), (ADDR, PORT))?;
+    println!("client: sent data to {}", ADDR);
+
+    let mut buf = vec![0; 4096];    //zero init 4096els in vec
+    let (len, remote_addr) = socket.recv_from(&mut buf)?;
+    let response = String::from_utf8_lossy(&buf[..len]);
+
+    println!("client: got data {:?} from {}", response, remote_addr);
+
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+    pub use std::{
+        env, io,
+        net::{Ipv4Addr, UdpSocket},
+        collections::LinkedList,
+    };
+
+    pub const ADDR: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 123);
+    pub const PORT: u16 = 7645;
+
+    use serde_derive::*;
+
+    #[derive(Serialize, Deserialize)]
+    pub enum Command{   //Client -> Daemon
+        Share{file_path: String,},
+        Scan{},
+        LS{},
+        Download{file_id: u32, save_path: String,},
+        //Download{file_name: String, save_path: String,},
+        Status{},
+    }
+
+    #[derive(Serialize, Deserialize)]
+    pub enum Answer{    //Daemon -> Client
+//      Scan{}, //Just update ls results?
+        LS{file_list: LinkedList<File>,},
+        Status{download_list: LinkedList<File>, shared_list: LinkedList<String>},
+    }//shared_list - just names of files, should I use File struct too?
+
+    #[derive(Serialize, Deserialize)]
+    pub struct File{
+        file_id: u32,   //Inactive in Anwer->Status case
+        file_name: String,
+        peer: String,   //(Or Ipv4Adress) "Remote" peer or client
+    }
