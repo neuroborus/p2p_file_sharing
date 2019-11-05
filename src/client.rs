63d7fad373c64ea974ef9098102834a12367398f
Mikhail Hasso <mihasso@protonmail.com>
2019-11-05 15:48:20 +0200

    Format

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,6 @@
 use lib::*;
-//socket == "channel"
-use clap::{Arg, App};
+
+use clap::{App, Arg};
 
 fn main() -> io::Result<()> {
     let mut stream = TcpStream::connect(("localhost", PORT_CLIENT_DAEMON)).unwrap();
@@ -9,50 +9,45 @@ fn main() -> io::Result<()> {
     //download "save_path" -fFileName (flag and save path in any order)
     //scan //ls //status
     let matches = App::new("ClientP2P")
-                          .about("Interaction with daemon")
-                          .arg(Arg::with_name("COMMAND")
-                          .required(true)
-                      )
-                          .arg(Arg::with_name("FLG_WAIT")
-                          .short("w")
-                      )
-                          .arg(Arg::with_name("FILE_NAME")    //Filename (download) - is option now
-                          .short("f")
-                          .takes_value(true)
-                      )
-                        /*  .arg(Arg::with_name("SAVE_PATH")    //Relative path
-                          .short("o")
-                          .takes_value(true)
-                      )*/
-                          .arg(Arg::with_name("FILE_PATH")
-                      )
-                          .get_matches();
+        .about("Interaction with daemon")
+        .arg(Arg::with_name("COMMAND").required(true))
+        .arg(Arg::with_name("FLG_WAIT").short("w"))
+        .arg(
+            Arg::with_name("FILE_NAME") //Filename (download) - is option now
+                .short("f")
+                .takes_value(true),
+        )
+        /*  .arg(Arg::with_name("SAVE_PATH")    //Relative path
+            .short("o")
+            .takes_value(true)
+        )*/
+        .arg(Arg::with_name("FILE_PATH"))
+        .get_matches();
 
-    let mut flag: bool = true;  //Now we don't need panic!
-    match matches.value_of("COMMAND").unwrap(){
+    let mut flag: bool = true; //Now we don't need panic!
+    match matches.value_of("COMMAND").unwrap() {
         "share" => {
-            if !matches.is_present("FILE_PATH"){
+            if !matches.is_present("FILE_PATH") {
                 println!("No path for sharing!");
                 flag = false;
             }
             //println!("\n\n\tshare\n");
             //////////
-            if flag{
+            if flag {
                 let f_path = PathBuf::from(matches.value_of("FILE_PATH").unwrap());
 
-                if f_path.is_file(){ //Checking the file
-                    let com = Command::Share{file_path: f_path};
+                if f_path.is_file() {
+                    //Checking the file
+                    let com = Command::Share { file_path: f_path };
                     //
                     let serialized = serde_json::to_string(&com)?;
                     stream.write(serialized.as_bytes()).unwrap();
-                }
-                else{
+                } else {
                     println!("File does not exists!");
                     flag = false;
                 }
-
             }
-        },
+        }
         "scan" => {
             //println!("\n\n\tscan\n");
             //////////
@@ -60,7 +55,7 @@ fn main() -> io::Result<()> {
             //
             let serialized = serde_json::to_string(&com)?;
             stream.write(serialized.as_bytes()).unwrap();
-        },
+        }
         "ls" => {
             //println!("\n\n\tls\n");
             //////////
@@ -68,19 +63,18 @@ fn main() -> io::Result<()> {
             //
             let serialized = serde_json::to_string(&com)?;
             stream.write(serialized.as_bytes()).unwrap();
-        },
+        }
         "download" => {
-            if !matches.is_present("FILE_NAME"){
+            if !matches.is_present("FILE_NAME") {
                 println!("No file name to download!");
                 flag = false;
             }
             //
-            if flag{
+            if flag {
                 let s_path: PathBuf;
-                if matches.is_present("FILE_PATH"){
+                if matches.is_present("FILE_PATH") {
                     s_path = PathBuf::from(matches.value_of("FILE_PATH").unwrap());
-                }
-                else{
+                } else {
                     s_path = PathBuf::from("");
                 }
                 //
@@ -88,17 +82,26 @@ fn main() -> io::Result<()> {
                 //println!("\n\n\tls\n");
                 //////////
                 let com: Command;
-                if matches.is_present("FLG_WAIT"){  //If user wants to block console until download is finished
-                    com = Command::Download{file_name: f_name, save_path: s_path, wait: true};
-                }else{
-                    com = Command::Download{file_name: f_name, save_path: s_path, wait: false};
+                if matches.is_present("FLG_WAIT") {
+                    //If user wants to block console until download is finished
+                    com = Command::Download {
+                        file_name: f_name,
+                        save_path: s_path,
+                        wait: true,
+                    };
+                } else {
+                    com = Command::Download {
+                        file_name: f_name,
+                        save_path: s_path,
+                        wait: false,
+                    };
                 }
 
                 //
                 let serialized = serde_json::to_string(&com)?;
                 stream.write(serialized.as_bytes()).unwrap();
             }
-        },
+        }
         "status" => {
             //println!("\n\n\tstatus\n");
             //////////
@@ -106,21 +109,20 @@ fn main() -> io::Result<()> {
             //
             let serialized = serde_json::to_string(&com)?;
             stream.write(serialized.as_bytes()).unwrap();
-        },
+        }
         _ => {
             println!("Wrong command!");
             flag = false;
         }
-
     }
 
-    if flag{
+    if flag {
         let mut buf = vec![0 as u8; 4096];
         match stream.read(&mut buf) {
             Ok(size) => {
                 let answ: Answer = serde_json::from_slice(&buf[..size])?;
                 println!("{:?}", answ);
-            },
+            }
             Err(_) => {
                 println!("An error occurred, {}", stream.peer_addr().unwrap());
             }
--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,80 @@
-    pub use std::{
-        env, io,
-        thread,
-        path::PathBuf,
-        sync::{Arc, Mutex, MutexGuard, mpsc, mpsc::{Sender, Receiver},},
-        io::{Read, Write},
-        net::{Ipv4Addr, UdpSocket, SocketAddr, TcpStream, TcpListener, Shutdown},
-        collections::{LinkedList, HashMap},
-    };
-    //pub enum c_type{share}
+pub use std::{
+    collections::{HashMap, LinkedList},
+    env, io,
+    io::{Read, Write},
+    net::{Ipv4Addr, Shutdown, SocketAddr, TcpListener, TcpStream, UdpSocket},
+    path::PathBuf,
+    sync::{
+        mpsc,
+        mpsc::{Receiver, Sender},
+        Arc, Mutex, MutexGuard,
+    },
+    thread,
+};
+//pub enum c_type{share}
 
-    pub const ADDR_DAEMON: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 123);
-    pub const ADDR_DAEMON_MULTICAST: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 124);
-    pub const PORT_CLIENT_DAEMON: u16 = 7645;
-    pub const PORT_MULTICAST: u16 = 7646;
-    pub const SCAN_REQUEST: &[u8; 20] = b"UDP_Scan_Request_P2P";
+pub const ADDR_DAEMON: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 123);
+pub const ADDR_DAEMON_MULTICAST: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 124);
+pub const PORT_CLIENT_DAEMON: u16 = 7645;
+pub const PORT_MULTICAST: u16 = 7646;
+pub const SCAN_REQUEST: &[u8; 20] = b"UDP_Scan_Request_P2P";
 
-    use serde_derive::*;
+use serde_derive::*;
 
-    #[derive(Serialize, Deserialize, Debug)]
-    pub enum Command{   //Client -> Daemon
-        Share{file_path: PathBuf,},
-        Scan,
-        Ls,
-        Download{file_name: String, save_path: PathBuf, wait: bool},
-        Status,
-    }
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Command {
+    //Client -> Daemon
+    Share {
+        file_path: PathBuf,
+    },
+    Scan,
+    Ls,
+    Download {
+        file_name: String,
+        save_path: PathBuf,
+        wait: bool,
+    },
+    Status,
+}
 
-    #[derive(Serialize, Deserialize, Debug)]
-    pub enum Answer{    //Daemon -> Client
-//      Scan{}, //Just update ls results?
-        Ok,
-        Ls{available_map: HashMap<String, Vec<SocketAddr>>}, //available
-        Status{transferring_map: HashMap<String, Vec<SocketAddr>>,
-            shared_map: HashMap<String, PathBuf>},
-    }
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Answer {
+    //Daemon -> Client
+    //      Scan{}, //Just update ls results?
+    Ok,
+    Ls {
+        available_map: HashMap<String, Vec<SocketAddr>>,
+    }, //available
+    Status {
+        transferring_map: HashMap<String, Vec<SocketAddr>>,
+        shared_map: HashMap<String, PathBuf>,
+    },
+}
 
-    #[derive(Debug)]
-    pub struct DataTemp{
+#[derive(Debug)]
+pub struct DataTemp {
     //Available to downloading files: name_of_file--shared IP addresses
-        pub available: HashMap<String, Vec<SocketAddr>>,
-        pub downloading: HashMap<String, Vec<SocketAddr>>,    //Already downloading
-        //
+    pub available: HashMap<String, Vec<SocketAddr>>,
+    pub downloading: HashMap<String, Vec<SocketAddr>>, //Already downloading
+    //
     //Your files, that available to transfer
-        pub shared: HashMap<String, PathBuf>,    //FileName - Path
-        pub transferring: HashMap<String, Vec<SocketAddr>>    //Already transferring
-    }
-    impl DataTemp{
-        pub fn new()->Self{
-            DataTemp{
-                available: HashMap::new(),
-                downloading: HashMap::new(),
-                shared: HashMap::new(),
-                transferring: HashMap::new(),
-            }
+    pub shared: HashMap<String, PathBuf>, //FileName - Path
+    pub transferring: HashMap<String, Vec<SocketAddr>>, //Already transferring
+}
+impl DataTemp {
+    pub fn new() -> Self {
+        DataTemp {
+            available: HashMap::new(),
+            downloading: HashMap::new(),
+            shared: HashMap::new(),
+            transferring: HashMap::new(),
         }
     }
+}
 
-    /*#[derive(Serialize, Deserialize, Debug)]
-    pub struct File{
-        //file_id: u32,   //Inactive in Answer->Status case
-        file_name: String,
-        peer: String,   //(Or Ipv4Adress) "Remote" peer or client
-    }*/
+/*#[derive(Serialize, Deserialize, Debug)]
+pub struct File{
+    //file_id: u32,   //Inactive in Answer->Status case
+    file_name: String,
+    peer: String,   //(Or Ipv4Adress) "Remote" peer or client
+}*/
